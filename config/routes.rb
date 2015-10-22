ed117418ff1420421a249feffb2cdeb37c798030
Dan Klos <dsklos@gmail.com>
2015-10-22 11:20:50 -0700

    Basic functionality is in place, refactoring for few buttons exposed to user, and adding in timestamps

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'welcome/index'
+
+
+  Rails.application.routes.draw do
+    resources :articles do
+      resources :comments
+    end
+    root 'welcome#index'
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
