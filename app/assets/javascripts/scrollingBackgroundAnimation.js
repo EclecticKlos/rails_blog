928ef72ef7ccc668d7ccefe3eed484e5f7d840a1
Dan Klos <dsklos@gmail.com>
2015-11-24 12:28:49 -0800

    Got the event to trigger successfully after scrolled into view, now merely need to make the eveent contained to only when above the canvas. So close.

--- app/assets/javascripts/scrollingBackgroundAnimation.js
+++ app/assets/javascripts/scrollingBackgroundAnimation.js
@@ -1,7 +1,6 @@
-var totalImages = IMAGE_URLS.length; // Wow, so many images for such a short clip
+var totalImages = IMAGE_URLS.length;
 var images = new Array();
 for(var i = 0; i < totalImages; i++) {
-    // var filename = 'images/movie_' + i + '.jpg'; // Filename of each image
     var img = new Image;
     img.src = IMAGE_URLS[i];
     images.push(img);
@@ -14,15 +13,28 @@ var context;
 $(document).ready(function(){
   canv = document.getElementById('background');
   context = canv.getContext('2d');
-  mouseWheel();
 
-  // See above for where this gets called
+  var distanceToCanvas = $('.full_screen_second').offset().top,
+    $window = $(window);
 
+  var indicator;
+  $window.scroll(function() {
+    if ( $window.scrollTop() <= distanceToCanvas ) {
+        console.log("Inside scroll")
+      if (!!window.mouseWheel){
+        removeMouseWheel();
+      }
+    }
+    else {
+      console.log("Calling mousewheel")
+      addMouseWheel();
+    }
+  })
 });
 
-var mouseWheel = function() {
-  window.addEventListener('mousewheel', function(e) {
-    console.log("HERE")
+var removeMouseWheel = function() {
+  window.removeEventListener('mousewheel', function(e) {
+    console.log("mouseWheel in background.js")
     e.preventDefault(); // No scroll
 
     // The following equation will return either a 1 for scroll down
@@ -30,8 +42,29 @@ var mouseWheel = function() {
     var delta = Math.max(-1, Math.min(1, e.wheelDelta));
 
     // This code mostly keeps us from going too far in either direction
-    if(delta == -1) currentLocation += .5;
-    if(delta == 1) currentLocation -= .5;
+    if(delta == -1) currentLocation += 1;
+    if(delta == 1) currentLocation -= 1;
+    if(currentLocation < 0) currentLocation = 0;
+    if(currentLocation > images.length)
+      currentLocation = images.length;
+
+    // See below for the details of this function
+    setImage(currentLocation);
+  });
+}
+
+var addMouseWheel = function() {
+  window.addEventListener('mousewheel', function(e) {
+    console.log("mouseWheel in background.js")
+    e.preventDefault(); // No scroll
+
+    // The following equation will return either a 1 for scroll down
+    // or -1 for a scroll up
+    var delta = Math.max(-1, Math.min(1, e.wheelDelta));
+
+    // This code mostly keeps us from going too far in either direction
+    if(delta == -1) currentLocation += 1;
+    if(delta == 1) currentLocation -= 1;
     if(currentLocation < 0) currentLocation = 0;
     if(currentLocation > images.length)
       currentLocation = images.length;
--- public/assets/application-59485cdd5a847af6c0620e891304fad4fdf819cc486ad84af626f841288297fb.js
+++ public/assets/application-59485cdd5a847af6c0620e891304fad4fdf819cc486ad84af626f841288297fb.js
@@ -11690,53 +11690,52 @@ $('.sidebar')
 
 
 }).call(this);
-// var totalImages = IMAGE_URLS.length; // Wow, so many images for such a short clip
-// var images = new Array();
-// for(var i = 0; i < totalImages; i++) {
-//     // var filename = 'images/movie_' + i + '.jpg'; // Filename of each image
-//     var img = new Image;
-//     img.src = IMAGE_URLS[i];
-//     images.push(img);
-// }
+var totalImages = IMAGE_URLS.length;
+var images = new Array();
+for(var i = 0; i < totalImages; i++) {
+    var img = new Image;
+    img.src = IMAGE_URLS[i];
+    images.push(img);
+}
 
 
-// var currentLocation = 0;
-// var canv;
-// var context;
-// $(document).ready(function(){
-//   canv = document.getElementById('background');
-//   context = canv.getContext('2d');
-//   mouseWheel();
+var currentLocation = 0;
+var canv;
+var context;
+$(document).ready(function(){
+  canv = document.getElementById('background');
+  context = canv.getContext('2d');
+  mouseWheel();
 
-//   // See above for where this gets called
+  // See above for where this gets called
 
-// });
+});
 
-// var mouseWheel = function() {
-//   window.addEventListener('mousewheel', function(e) {
-//     console.log("HERE")
-//     e.preventDefault(); // No scroll
+var mouseWheel = function() {
+  window.addEventListener('mousewheel', function(e) {
+    console.log("HERE")
+    e.preventDefault(); // No scroll
 
-//     // The following equation will return either a 1 for scroll down
-//     // or -1 for a scroll up
-//     var delta = Math.max(-1, Math.min(1, e.wheelDelta));
+    // The following equation will return either a 1 for scroll down
+    // or -1 for a scroll up
+    var delta = Math.max(-1, Math.min(1, e.wheelDelta));
 
-//     // This code mostly keeps us from going too far in either direction
-//     if(delta == -1) currentLocation += 1;
-//     if(delta == 1) currentLocation -= 1;
-//     if(currentLocation < 0) currentLocation = 0;
-//     if(currentLocation > images.length)
-//       currentLocation = images.length;
+    // This code mostly keeps us from going too far in either direction
+    if(delta == -1) currentLocation += .5;
+    if(delta == 1) currentLocation -= .5;
+    if(currentLocation < 0) currentLocation = 0;
+    if(currentLocation > images.length)
+      currentLocation = images.length;
 
-//     // See below for the details of this function
-//     setImage(currentLocation);
-//   });
-// }
+    // See below for the details of this function
+    setImage(currentLocation);
+  });
+}
 
-// var setImage = function(newLocation) {
-//     // drawImage takes 5 arguments: image, x, y, width, height
-//     context.drawImage(images[newLocation], 0, 0, 1280, 720);
-//   }
+var setImage = function(newLocation) {
+    // drawImage takes 5 arguments: image, x, y, width, height
+    context.drawImage(images[newLocation], 0, 0, 1280, 720);
+}
 ;
 /*!
  * skrollr core
@@ -13522,6 +13521,250 @@ $('.sidebar')
 
 
 }).call(this);
+/**
+ * Generates event when user makes new movement (like a swipe on a touchscreen).
+ * @version 1.1.1
+ * @link https://github.com/Promo/wheel-indicator
+ * @license MIT
+ */
+
+/* global module, window, document */
+
+
+var WheelIndicator = (function(win, doc) {
+    var eventWheel = 'onwheel' in doc ? 'wheel' : 'mousewheel',
+
+        DEFAULTS = {
+            callback: function(){},
+            elem: win,
+            preventMouse: true
+        };
+
+    function Module(options){
+        this._options = extend(DEFAULTS, options);
+        this._deltaArray = [ 0, 0, 0 ];
+        this._isAcceleration = false;
+        this._isStopped = true;
+        this._direction = '';
+        this._timer = '';
+        this._isWorking = true;
+
+        var self = this;
+        this._wheelHandler = function(event) {
+            if (self._isWorking) {
+                processDelta.call(self, event);
+
+                if (self._options.preventMouse) {
+                  preventDefault(event);
+                }
+            }
+        };
+
+        addEvent(this._options.elem, eventWheel, this._wheelHandler);
+    }
+
+    Module.prototype = {
+        constructor: Module,
+
+        turnOn: function(){
+            this._isWorking = true;
+
+            return this;
+        },
+
+        turnOff: function(){
+            this._isWorking = false;
+
+            return this;
+        },
+
+        setOptions: function(options){
+            this._options = extend(this._options, options);
+
+            return this;
+        },
+
+        getOption: function(option){
+            var neededOption = this._options[option];
+
+            if (neededOption !== undefined) {
+              return neededOption;
+            }
+
+            throw new Error('Unknown option');
+        },
+
+        destroy: function(){
+            removeEvent(this._options.elem, eventWheel, this._wheelHandler);
+
+            return this;
+        }
+    };
+
+    function triggerEvent(event){
+        event.direction = this._direction;
+
+        this._options.callback.call(this, event);
+    }
+
+    var getDeltaY = function(event){
+        if (event.wheelDelta) {
+            getDeltaY = function(event) {
+                return event.wheelDelta * -1;
+            };
+        } else {
+            getDeltaY = function(event) {
+                return event.deltaY;
+            };
+        }
+        //return x3 for ya
+        return getDeltaY(event);
+    };
+
+    function preventDefault(event){
+        event = event || win.event;
+
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    }
+
+    function processDelta(event) {
+        var
+            self = this,
+            delta = getDeltaY(event),
+            direction = delta > 0 ? 'down' : 'up',
+            arrayLength = self._deltaArray.length,
+            changedDirection = false,
+            repeatDirection = 0,
+            sustainableDirection, i;
+
+        clearTimeout(self._timer);
+
+        self._timer = setTimeout(function() {
+            self._deltaArray = [ 0, 0, 0 ];
+            self._isStopped = true;
+            self._direction = direction;
+        }, 150);
+
+        //check how many of last three deltas correspond to certain direction
+        for(i = 0; i < arrayLength; i++) {
+            if(self._deltaArray[i] !== 0) {
+                self._deltaArray[i] > 0 ? ++repeatDirection : --repeatDirection;
+            }
+        }
+
+        //if all of last three deltas is greater than 0 or lesser than 0 then direction is switched
+        if(Math.abs(repeatDirection) === arrayLength) {
+            //determine type of sustainable direction
+            //(three positive or negative deltas in a row)
+            sustainableDirection = repeatDirection > 0 ? 'down' : 'up';
+
+            if(sustainableDirection !== self._direction) {
+                //direction is switched
+                changedDirection = true;
+                self._direction = direction;
+            }
+        }
+
+        //if wheel`s moving and current event is not the first in array
+        if(!self._isStopped){
+            if(changedDirection) {
+                self._isAcceleration = true;
+
+                triggerEvent.call(this, event);
+            } else {
+                //check only if movement direction is sustainable
+                if(Math.abs(repeatDirection) === arrayLength) {
+                    //must take deltas to don`t get a bug
+                    //[-116, -109, -103]
+                    //[-109, -103, 1] - new impulse
+
+                    analyzeArray.call(this, event);
+                }
+            }
+        }
+
+        //if wheel is stopped and current delta value is the first in array
+        if(self._isStopped) {
+            self._isStopped = false;
+            self._isAcceleration = true;
+            self._direction = direction;
+
+            triggerEvent.call(this, event);
+        }
+
+        self._deltaArray.shift();
+        self._deltaArray.push(delta);
+    }
+
+    function analyzeArray(event) {
+        var
+            deltaArray0Abs  = Math.abs(this._deltaArray[0]),
+            deltaArray1Abs  = Math.abs(this._deltaArray[1]),
+            deltaArray2Abs  = Math.abs(this._deltaArray[2]),
+            deltaAbs        = Math.abs(getDeltaY(event));
+
+        if((deltaAbs       > deltaArray2Abs) &&
+            (deltaArray2Abs > deltaArray1Abs) &&
+            (deltaArray1Abs > deltaArray0Abs)) {
+
+            if(!this._isAcceleration) {
+                triggerEvent.call(this, event);
+                this._isAcceleration = true;
+            }
+        }
+
+        if((deltaAbs < deltaArray2Abs) &&
+            (deltaArray2Abs <= deltaArray1Abs)) {
+            this._isAcceleration = false;
+        }
+    }
+
+    function addEvent(elem, type, handler){
+        if(elem.addEventListener) {
+            elem.addEventListener(type, handler, false);
+        } else if (elem.attachEvent) {
+            elem.attachEvent('on' + type, handler);
+        }
+    }
+
+    function removeEvent(elem, type, handler) {
+        if (elem.removeEventListener) {
+            elem.removeEventListener(type, handler, false);
+        } else if (elem.detachEvent) {
+            elem.detachEvent('on'+ type, handler);
+        }
+    }
+
+    function extend(defaults, options) {
+        var extended = {},
+            prop;
+
+        for (prop in defaults) {
+            if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
+                extended[prop] = defaults[prop];
+            }
+        }
+
+        for (prop in options) {
+            if (Object.prototype.hasOwnProperty.call(options, prop)) {
+                extended[prop] = options[prop];
+            }
+        }
+
+        return extended;
+    }
+
+    return Module;
+}(window, document));
+
+if (typeof exports === 'object') {
+    module.exports = WheelIndicator;
+}
+;
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
